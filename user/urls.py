2973d59c56387bcc295105a0f2de03867996cb68
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2020-11-22 19:28:40 +0530

    edit and delete functionality done

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home_view, name='home_view'),
-    path('add/', views.add_view, name='add_view')
+    path('add/', views.add_view, name='add_view'),
+    path('delete/<int:user_id>/', views.delete_view, name='delete_view'),
+    path('edit/<int:user_id>/', views.edit_view, name='edit_view')
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import User
 
 # Create your views here.
 def home_view(request):
     users = User.objects.all()
     if users is not None:
-        context = {
-            'users': users
-        }
+        context = {'users': users}
     else:
         context = {}
     return render(request, 'home_view.html', context)
@@ -25,4 +23,24 @@ def add_view(request):
         # print("Details: " + name + username + email + password + comments)
         user = User(name=name, username=username, email=email, password=password, comments=comments, image=image)
         user.save()
+        return redirect('home_view')
     return render(request, 'add_view.html')
+
+
+def delete_view(request, user_id):
+    user = User.objects.get(id=user_id)
+    user.delete()
+    return redirect('home_view')
+
+
+def edit_view(request, user_id):
+    user = User.objects.get(id=user_id)
+    context = {'user': user}
+    if request.method == 'POST':
+        user.name = request.POST.get('name')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.comments = request.POST.get('comments')
+        user.save()
+        return redirect('home_view')
+    return render(request, 'edit_view.html', context)
